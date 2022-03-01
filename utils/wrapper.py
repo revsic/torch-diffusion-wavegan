e1c569be467365346aa1611b76a5ab90ecf77f60
revsic <revsic99@gmail.com>
2022-03-01 03:31:46 +0000

    wrapper: Rename gt to base

--- utils/wrapper.py
+++ utils/wrapper.py
@@ -81,23 +81,23 @@ class TrainingWrapper:
         # [B, S x H]
         prev = prev_mean + torch.randn_like(prev_mean) * prev_std[:, None]
         # [B, S x H], [B]
-        gt_mean, gt_std = self.model.diffusion(prev, steps, next_=True)
+        base_mean, base_std = self.model.diffusion(prev, steps, next_=True)
         # [B, S x H]
-        gt = gt_mean + torch.randn_like(gt_mean) * gt_std[:, None]
+        base = base_mean + torch.randn_like(base_mean) * base_std[:, None]
         # [B, S x H]
-        disc_gt = self.disc(prev, gt, steps)
+        disc_gt = self.disc(prev, base, steps)
         # []
         loss_d = F.binary_cross_entropy_with_logits(
             disc_gt, torch.ones_like(disc_gt))
 
         # [B, S x H]
-        denoised = self.model.denoise(gt, torch.randn_like(gt), mel, steps)
+        denoised = self.model.denoise(base, torch.randn_like(base), mel, steps)
         # [B, S x H], [B]
         pred_mean, pred_std = self.model.diffusion(denoised, steps - 1)
         # [B, S x H]
         pred = pred_mean + torch.randn_like(pred_mean) * pred_std[:, None]
         # [B, S x H]
-        disc_pred = self.disc(pred, gt, steps)
+        disc_pred = self.disc(pred, base, steps)
         # []
         loss_g = F.binary_cross_entropy_with_logits(
             disc_pred, torch.zeros_like(disc_pred))
@@ -107,7 +107,7 @@ class TrainingWrapper:
             'dloss': loss.item(),
             'dloss_d': loss_d.item(), 'dloss_g': loss_g.item()}
         return loss, losses, {
-            'gt': gt.cpu().detach().numpy(),
+            'base': base.cpu().detach().numpy(),
             'prev': prev.cpu().detach().numpy(),
             'denoised': denoised.cpu().detach().numpy(),
             'pred': pred.cpu().detach().numpy()}
@@ -125,22 +125,22 @@ class TrainingWrapper:
         steps = torch.randint(
             self.config.model.steps, (mel.shape[0],), device=mel.device)
         # [B, S x H], [B]
-        gt_mean, gt_std = self.model.diffusion(speech, steps)
+        base_mean, base_std = self.model.diffusion(speech, steps)
         # [B, S x H]
-        gt = gt_mean + torch.randn_like(gt_mean) * gt_std[:, None]
+        base = base_mean + torch.randn_like(base_mean) * base_std[:, None]
         # [B, S x H]
-        denoised = self.model.denoise(gt, torch.randn_like(gt), mel, steps)
+        denoised = self.model.denoise(base, torch.randn_like(base), mel, steps)
         # [B, S x H], [B]
         pred_mean, pred_std = self.model.diffusion(denoised, steps - 1)
         # [B, S x H]
         pred = pred_mean + torch.randn_like(pred_mean) * pred_std[:, None]
         # [B, S x H]
-        disc_pred = self.disc(pred, gt, steps)
+        disc_pred = self.disc(pred, base, steps)
         # []
         loss = F.binary_cross_entropy_with_logits(
             disc_pred, torch.ones_like(disc_pred))
         losses = {'gloss': loss.item()}
         return loss, losses, {
-            'gt': gt.cpu().detach().numpy(),
+            'base': base.cpu().detach().numpy(),
             'denoised': denoised.cpu().detach().numpy(),
             'pred': pred.cpu().detach().numpy()}
